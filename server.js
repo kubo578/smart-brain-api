4d545f245bc2388db42f0acb03bb93e8e4808dbe
kubo578 <chass.matharu@gmail.com>
2020-05-19 07:26:48 +0100

    connecting the front/ backend - signin

--- server.js
+++ server.js
@@ -1,37 +1,47 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt  = require('bcrypt-nodejs');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const database = {
 	users: [
 	  {
 	  	id: '001',
 	  	name: 'John',
+	  	password: 'John',
 	  	email: 'john@gmail.com',
-	  	password:'John',
 	  	entries: 0,
 	  	joined: new Date()
 	  },
 	  {
 	  	id: '002',
 	  	name: 'Sally',
+	  	password: 'Sally',
 	  	email: 'sally@gmail.com',
-	  	password:'Sally',
 	  	entries: 0,
 	  	joined: new Date()
 	  }
+	],
+	logins: [
+	{
+		id:'001',
+		hash:'',
+		email:'john@gmail.com'
+	}
 	]
 }
 
 
 app.get('/', (req, res) => {
-	res.json(database.users);
+	res.send(database.users);
 });
 
 app.get('/profile/:userId', (req, res) => {
-    const id = req.params.userId;
+    const { id } = req.params;
     database.users.forEach( user => {
     	if (user.id === id){
     		return res.status(200).json(user)
@@ -41,29 +51,46 @@ app.get('/profile/:userId', (req, res) => {
 });
 
 app.post('/signin', (req, res) => {
-	if (req.body.email === database.users[0].email &&
-		req.body.password === database.users[0].password) {
-		res.status(200).json('success');
+	const { email, password } = req.body;
+	let found = false;
+	let  index = database.logins.findIndex(login => login.email === email)
+	if (index == 1) {
+   	    bcrypt.compare(password, database.logins[index].hash, function(err, response) {
+           if (response == true) {
+            	return res.status(200).json('success');
+            } else {
+            	return res.status(400).json('error loginning in')
+            }
+        });
 	} else {
-		res.status(400).json('User does not exist')
+	    return res.status(400).json('error loginning in')
 	}
 });
 
 app.post('/register', (req, res) => {
+	const { name, email, password } = req.body;
+	bcrypt.hash(password, null, null, function(err, hash) {
+	    database.logins.push(
+	    {
+	   		id: '003',
+	   		email: email,
+	   		hash: hash
+	    });
+    });
+
 	database.users.push(
 	{
 	  	id: '003',
-	  	name: req.body.name,
-	  	email: req.body.email,
-	  	password:req.body.password,
+	  	name: name,
+	  	email: email,
 	  	entries: 0,
 	  	joined: new Date()
 	})
 	res.status(200). json('user 003 added');
 })
 
-app.put('/image/:userId', (req, res) => {
-    const id = req.params.userId;
+app.put('/image/', (req, res) => {
+    const { id } = req.body;
     let found = false
     database.users.forEach( user => {
     	if (user.id === id) {
